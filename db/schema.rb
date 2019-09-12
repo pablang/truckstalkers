44756c7a150fb8e688d4fb9abb31314afb5a48e1
Pamela <pamablang@gmail.com>
2019-09-13 00:52:40 +1000

    Add signup and login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_095933) do
+ActiveRecord::Schema.define(version: 2019_09_12_135515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2019_09_12_095933) do
     t.text "facebook"
     t.text "twitter"
     t.text "instagram"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
