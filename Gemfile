43c32a8d6aedd227a9611319a77d815e3d37e288
Pamela <pamablang@gmail.com>
2019-09-24 20:09:44 +1000

    Use friendly urls

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'kaminari'
 gem 'geocoder'
 # Use for creating tags/categories
 gem 'acts-as-taggable-on', '~> 6.0'
+# Used for creating friendly urls
+gem 'friendly_id', '~> 5.2.4'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_131809) do
+ActiveRecord::Schema.define(version: 2019_09_24_091743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.integer "truck_id"
@@ -83,6 +94,8 @@ ActiveRecord::Schema.define(version: 2019_09_23_131809) do
     t.string "address"
     t.string "timings"
     t.integer "average_cost_for_two"
+    t.string "slug"
+    t.index ["slug"], name: "index_trucks_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
